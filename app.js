acd9ea2968764dbacf48ecf79bd8dbfa22a51359
Gopal Meena <meenagopal24@gmail.com>
2023-06-10 00:34:57 +0530

    com

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const category = require("./routes/category");
 const wallpaper = require("./routes/wallpapers");
 
 app.use("/wallpaper", wallpaper);
-app.use("/category",category)
-
-app.listen(3000, () => {
-  console.log("listning 3000");
+app.use("/category", category);
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
 });
