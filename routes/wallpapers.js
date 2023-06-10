f4cda0190bf54c628e3ba99413d474e3a6a45286
Gopal Meena <meenagopal24@gmail.com>
2023-06-11 01:34:36 +0530

    new wallpapers added

--- routes/wallpapers.js
+++ routes/wallpapers.js
@@ -6,6 +6,23 @@ const router = express.Router();
 router.get("/", async (request, response) => {
   const wallpapers = [
     {
+      url: "https://images.pexels.com/photos/1366913/pexels-photo-1366913.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/3881034/pexels-photo-3881034.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/568948/pexels-photo-568948.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg",
+      name: "Random Name",
+    },
+
+    {
       url: "https://images.pexels.com/photos/1420226/pexels-photo-1420226.jpeg",
       name: "Random Name",
     },
@@ -43,41 +60,41 @@ router.get("/", async (request, response) => {
 
 //for getting category wise wallapaper
 router.get("/:category", async (request, response) => {
-    const wallpapers = [
-        {
-          url: "https://images.pexels.com/photos/1420226/pexels-photo-1420226.jpeg",
-          name: "Random Name",
-        },
-        {
-          url: "https://images.pexels.com/photos/6691950/pexels-photo-6691950.jpeg",
-          name: "Random Name",
-        },
-        {
-          url: "https://images.pexels.com/photos/14286814/pexels-photo-14286814.jpeg",
-          name: "Random Name",
-        },
-        {
-          url: "https://images.pexels.com/photos/11200581/pexels-photo-11200581.jpeg",
-          name: "Random Name",
-        },
-        {
-          url: "https://images.pexels.com/photos/10585143/pexels-photo-10585143.jpeg",
-          name: "Random Name",
-        },
-        {
-          url: "https://images.pexels.com/photos/5846273/pexels-photo-5846273.jpeg",
-          name: "Random Name",
-        },
-        {
-          url: "https://images.pexels.com/photos/11200581/pexels-photo-11200581.jpeg",
-          name: "Random Name",
-        },
-        {
-          url: "https://images.pexels.com/photos/13530493/pexels-photo-13530493.jpeg",
-          name: "Random Name",
-        },
-      ];
-      response.send({ wallpapers });
+  const wallpapers = [
+    {
+      url: "https://images.pexels.com/photos/1420226/pexels-photo-1420226.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/6691950/pexels-photo-6691950.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/14286814/pexels-photo-14286814.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/11200581/pexels-photo-11200581.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/10585143/pexels-photo-10585143.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/5846273/pexels-photo-5846273.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/11200581/pexels-photo-11200581.jpeg",
+      name: "Random Name",
+    },
+    {
+      url: "https://images.pexels.com/photos/13530493/pexels-photo-13530493.jpeg",
+      name: "Random Name",
+    },
+  ];
+  response.send({ wallpapers });
 });
 
 module.exports = router;
