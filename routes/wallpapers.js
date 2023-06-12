4284b9b59b6c12f8eec45229d8771c94fc01ce39
Gopal Meena <meenagopal24@gmail.com>
2023-06-13 01:37:17 +0530

    uuid added

--- routes/wallpapers.js
+++ routes/wallpapers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 //for getting recent wallpapers
 router.get("/", async (request, response) => {
@@ -8,51 +9,63 @@ router.get("/", async (request, response) => {
     {
       url: "https://images.pexels.com/photos/1366913/pexels-photo-1366913.jpeg",
       name: "Random Name 1",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/3881034/pexels-photo-3881034.jpeg",
       name: "Random Name 2",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/568948/pexels-photo-568948.jpeg",
       name: "Random Name 3",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg",
       name: "Random Name 4",
+      uuid : uuidv4()
     },
 
     {
       url: "https://images.pexels.com/photos/1420226/pexels-photo-1420226.jpeg",
       name: "Random Name 5",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/6691950/pexels-photo-6691950.jpeg",
       name: "Random Name 6",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/14286814/pexels-photo-14286814.jpeg",
       name: "Random Name",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/11200581/pexels-photo-11200581.jpeg",
       name: "Random Name",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/10585143/pexels-photo-10585143.jpeg",
       name: "Random Name",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/5846273/pexels-photo-5846273.jpeg",
       name: "Random Name",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/11200581/pexels-photo-11200581.jpeg",
       name: "Random Name",
+      uuid : uuidv4()
     },
     {
       url: "https://images.pexels.com/photos/13530493/pexels-photo-13530493.jpeg",
       name: "Random Name",
+      uuid : uuidv4()
     },
   ];
   response.send({ wallpapers });
